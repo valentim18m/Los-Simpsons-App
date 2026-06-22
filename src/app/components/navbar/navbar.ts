6603b256f1741bd81898d4e9ad4ffe2b15f9d866
Valentin <valentinmaure2018@gmail.com>
2026-06-22 13:39:00 -0300

    actualizamos funciones de spartan

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HlmButtonImports],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {}
